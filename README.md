87e544debf18eaa3a812b3ac10ffd6367f4e4564
Adair Hawkins <adairhawkins@gmail.com>
2017-09-12 12:31:46 -0500

    Added pages

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # EventGuru
 Group #5 project
+
+"How to make carousel cycle" https://v4-alpha.getbootstrap.com/components/carousel/#carouselcycle 
---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+ var config = {
+    apiKey: "AIzaSyAMUimYAFDg4Bw-b3hLlYtPYCZW2QGtNE0",
+    authDomain: "event-guru-13a23.firebaseapp.com",
+    databaseURL: "https://event-guru-13a23.firebaseio.com",
+    projectId: "event-guru-13a23",
+    storageBucket: "",
+    messagingSenderId: "924347251636"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  var userName = "";
+
+$("#log-in").on("click", function(){
+	event.preventDefault();
+	userName = $("#user-name").val();
+	
+	var userToAdd = {
+		username: userName		
+	}
+	database.ref().push(userToAdd);
+
+});
+
+
+function newEvent (place, time, date) {
+	this.place = place;
+	this.time = time;
+	this.date = date;
+}
+
+$("#create-event").on("click", function(){
+	var calEvent = new newEvent(x,y,z);
+});
+
+$("#sign-in").on("click", function(){
+	console.log("You clicked me!");
+});
+
+$("#sign-in").on("click", function(){
+	$(".wrapper").hide();
+});
